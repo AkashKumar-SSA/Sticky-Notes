4b0f46b1e40afd30a4bf8727da68b996e89c2996
akash <akashkr6060@gmail.com>
2024-06-06 21:36:49 +0530

    the toolbar has been made in perfect format

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-let Add_notes = document.querySelector("#button");
-let text = document.getElementsByTagName("textarea")[0];
-let background_color = document.querySelector("#background_choose #color");
-let text_color = document.querySelector("#text_color_choose #color");
-let notes = document.querySelector("#notes_container");
-let empty_line = document.querySelector("#empty_line");
-let add_note = document.querySelector("#add_note");
-let text_size = document.querySelector("#text_size #size");
-let notes_heading = document.querySelector("#notes_heading");
-let ai_input = document.querySelector("#ai_input");
+const Add_notes = document.querySelector("#button");
+const text = document.getElementsByTagName("textarea")[0];
+const background_color = document.querySelector("#background_choose #color");
+const text_color = document.querySelector("#text_color #color");
+const notes = document.querySelector("#notes_container");
+const empty_line = document.querySelector("#empty_line");
+const add_note = document.querySelector("#add_note");
+const text_size = document.querySelector("#text_size");
+const notes_heading = document.querySelector("#notes_heading");
+const ai_input = document.querySelector("#ai_input");
 
-let notes_location_info = document.querySelector("#notes_location_info");
+const notes_location_info = document.querySelector("#notes_location_info");
 
 reset(); // Calling the reset function to reset all the containers
 
@@ -32,33 +32,33 @@ function AddNotes() {
     if (text.value === "") {
         return;
     }
-    if(notes_heading.value === ""){
+    if (notes_heading.value === "") {
         alert("Please give heading to your notes !");
         return;
     }
-    if(notes_location_info.value === ""){
+    if (notes_location_info.value === "") {
         alert("Please give location - where you are making your notes");
         return;
     }
 
     empty_line.innerText = "";
 
-    let div = document.createElement("div");
+    const div = document.createElement("div");
     div.id = "main_notes_container";
     // Styling the main div element
     div.style = "display:flex;flex-direction:column;min-width:400px;max-width:600px;height:350px; border-top-right-radius: 60px; border-bottom-left-radius: 60px;position:relative;";
     div.style.backgroundColor = background_color.value;
 
     // code for Cross Button/cross_div - cross button
-    let cross_div = document.createElement("div");
+    const cross_div = document.createElement("div");
     cross_div.id = "heading_cross_div"
     div.appendChild(cross_div);
-    let heading = document.createElement("p");
+    const heading = document.createElement("p");
     heading.id = "create_notes_heading";
     cross_div.appendChild(heading);
     heading.innerText = notes_heading.value;
 
-    let cross_btn = document.createElement("button");
+    const cross_btn = document.createElement("button");
     cross_div.appendChild(cross_btn);
 
     // Cross button creation
@@ -79,23 +79,23 @@ function AddNotes() {
     });
 
     // code for inserting text/text_div
-    let text_div = document.createElement("div");
+    const text_div = document.createElement("div");
     div.appendChild(text_div);
-    let content = document.createElement("p");
+    const content = document.createElement("p");
     text_div.appendChild(content);
     text_div.style = "margin:0";
     content.id = "sticky_note_content";
     content.innerText = text.value;
     content.style.fontSize = text_size.value + "px";
     content.style.color = text_color.value;
-    
+
 
     // creating date-time - location div element
-    let date_time_location_div = document.createElement("div");
+    const date_time_location_div = document.createElement("div");
     div.appendChild(date_time_location_div);
 
     // Clock logo
-    let clock_logo = document.createElement("img");
+    const clock_logo = document.createElement("img");
     clock_logo.src = "clock2_image.jpg";
     clock_logo.style = "height:25px;"
 
@@ -105,7 +105,7 @@ function AddNotes() {
 
     date_time_location_div.style = "height:fit-content;display:flex;flex-direction:row;justify-content:end;margin:0;padding:0;gap:20px;margin-right:10px;";
 
-    let date_time = document.createElement("p");
+    const date_time = document.createElement("p");
     date_time_only_div.appendChild(date_time);
     date_time.innerText = new Date().toLocaleString();
     date_time.style = "display:none;"
@@ -121,21 +121,21 @@ function AddNotes() {
     });
 
     // Location popup div
-    let location_popup_div = document.createElement("div");
+    const location_popup_div = document.createElement("div");
     location_popup_div.className = "location_popup_div";
     location_popup_div.id = "location_popup_div";
     div.appendChild(location_popup_div);
 
     // Mouseover / Mouseout event Location logo
-    let location_logo = document.createElement("img");
+    const location_logo = document.createElement("img");
     location_logo.src = "location_logo.jpg";
     location_logo.style = "height:25px;display:flex;flex-direction:column;";
     date_time_location_div.appendChild(location_logo);
-    let popup_heading = document.createElement("P");
+    const popup_heading = document.createElement("P");
     location_popup_div.appendChild(popup_heading);
     popup_heading.innerHTML = "<b>At :</b>";
 
-    let popup_location = document.createElement("p");
+    const popup_location = document.createElement("p");
     location_popup_div.appendChild(popup_location);
     popup_location.innerText = notes_location_info.value;
 
@@ -148,14 +148,14 @@ function AddNotes() {
     });
 
     // Notes edit-option code container
-    let edit_note = document.createElement("img");
+    const edit_note = document.createElement("img");
     edit_note.src = "edit_logo.jpg";
     edit_note.style = "height:25px;";
     date_time_location_div.appendChild(edit_note);
 
 
     // edit note popup div
-    let edit_note_popup_div = document.createElement("div");
+    const edit_note_popup_div = document.createElement("div");
     edit_note_popup_div.className = "edit_note_popup_div";
     edit_note_popup_div.id = "edit_note_popup_div";
     div.appendChild(edit_note_popup_div);
@@ -182,16 +182,16 @@ function AddNotes() {
     });
 
     notes.appendChild(div); // adding the div to the notes div container
-   
+
     //reseting the values 
     reset();
 }
-let refresh_content = document.querySelector("#refresh_content");
-refresh_content.addEventListener("click",()=>{
-    refresh_content.style = "Animation-name:rotate_refresh_content;animation-duration:5s;"
+const refresh_content = document.querySelector("#refresh_content");
+refresh_content.addEventListener("click", () => {
     reset();
 });
-function reset(){
+
+function reset() {
     notes_heading.value = "";
     text.value = ""
     text_size.value = "30";
@@ -201,30 +201,30 @@ function reset(){
     text.style.backgroundColor = "#223344";
     text.style.height = "300px";
     text.style.width = "70%";
-
     notes_location_info.value = "";
-    
+    text_color_choose_button.style.color = text_color.value;
+
 }
 //------------------------------------------------code separation line-------------------------------------------------------------
 // Code for integrating interactive Gemini AI_chatbot
 
-let ai_button = document.querySelector("#ai"); //AI button
+const ai_button = document.querySelector("#ai"); //AI button
 
 //question input and answer_generate button
 
-let ai_answer_generate_button = document.querySelector("#ai_answer_generate_button");
+const ai_answer_generate_button = document.querySelector("#ai_answer_generate_button");
 
-let ai_container = document.querySelector("#ai_container");
-let ai_output_container_div = document.querySelector("#ai_output_container_div");
-let ai_input_container_div = document.querySelector("#ai_input_container_div");
+const ai_container = document.querySelector("#ai_container");
+const ai_output_container_div = document.querySelector("#ai_output_container_div");
+const ai_input_container_div = document.querySelector("#ai_input_container_div");
 
-ai_input.addEventListener('keydown', function(event) {
+ai_input.addEventListener('keydown', function (event) {
     if (event.key === 'Enter') {
-    ai_answer_generate_button.click();
+        ai_answer_generate_button.click();
     }
-  });
+});
 
-  
+
 function userquestion() {
     return ai_input.value;
 }
@@ -257,10 +257,10 @@ function AI_QUERY() {
     if (ai_input.value == "") {
         return;
     }
-    let ai_output_question = document.querySelector("#ai_output_question");
+    const ai_output_question = document.querySelector("#ai_output_question");
     ai_output_question.innerText = ai_input.value + " ?";
 
-    let ai_output_container_div = document.querySelector("#ai_output_container_div");
+    const ai_output_container_div = document.querySelector("#ai_output_container_div");
     ai_output_container_div.style = "display:flex;"
     console.log(ai_input.value);
 
--- toolbar.js
+++ toolbar.js
@@ -1,36 +1,74 @@
 // code for toolbar -----------------------------------------------
 const toolbar = document.querySelector("#toolbar");
-const stickyNote = document.querySelector("#sticky-note p");
-const text_area = document.querySelector("#text_area")
+const size_input_button = document.querySelector("#size_input_button");
 
-toolbar.addEventListener("click", function (event) {
-    const buttonId = event.target.id;
-
-    switch (buttonId) {
-        case "bold":
-            document.execCommand("bold", false, null);
-            break;
-        case "italic":
-            document.execCommand("italic", false, null);
-            break;
-        case "underline":
-            document.execCommand("underline", false, null);
-            break;
-        case "strikethrough":
-            document.execCommand("strikethrough", false, null);
-            break;
-        case "color":
-            document.execCommand("ForeColor", false, color);
-            break;
-        case "size":
-
-            document.execCommand("FontSize", false, size);
-            break;
+let display_sizeInput = false;
+size_input_button.addEventListener("click", function () {
+    if (display_sizeInput) {
+        text_size.style = "display:none;"
+        display_sizeInput = false;
+    }
+    else {
+        document.querySelector("#text_size").style = "display:block;";
+        display_sizeInput = true;
     }
 });
 
-// stickyNote.addEventListener("input", function () {
-//     console.log(stickyNote.innerText);
-// });
+const text_background_color = document.querySelector("#text_background_color");
+const background_colord = document.querySelector("#background_choose #color");
+background_colord.addEventListener('change', function () {
+    text_background_color.style.backgroundColor = background_colord.value;
+});
 
-// ------------------------------------------------
+const text_color_choose_button = document.querySelector("#text_color_choose_button");
+
+text_color_choose_button.addEventListener("click", function () {
+    text_color.click();
+});
+text_color.addEventListener("change", () => {
+    text_color_choose_button.style.color = text_color.value;
+});
+
+function boldText() {
+    document.execCommand('bold', false, null);
+}
+
+function underlineText() {
+    document.execCommand('underline', false, null);
+}
+
+function strikethroughText() {
+    document.execCommand('strikethrough', false, null);
+}
+
+// background_color.addEventListener("click", () => { });
+
+// toolbar.addEventListener("click", function (event) {
+//     const buttonId = event.target.id;
+
+//     switch (buttonId) {
+//         case "bold":
+
+//             document.execCommand("bold", false, null);
+//             break;
+//         case "italic":
+//             document.execCommand("italic", false, null);
+//             break;
+//         case "underline":
+//             document.execCommand("underline", false, null);
+//             break;
+//         case "strikethrough":
+//             document.execCommand("strikethrough", false, null);
+//             break;
+//         case "text_color":
+//             document.execCommand("ForeColor", false, color);
+//             break;
+//         case "background_color":
+//             break;
+//         case "textSize_input_container":
+//             alert("enter text size")
+
+//             document.execCommand("FontSize", false, size);
+//             break;
+//     }
+// });
